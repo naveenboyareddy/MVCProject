e77a5f76d800c31d4382f5ea3521cd8e4be9b99e
LAPTOP-0H013EO4\Nvn <boyanaveen30@gmail.com>
2017-04-04 16:08:45 -0400

    commit

---
+++ Vidly1/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly1.Models;
+
+namespace Vidly1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/random
+        public ActionResult random()
+        {
+            var movie = new Movie() { Name = "SHARK!" };
+            return View(movie);
+        }
+        [Route("Movies/customers")]
+        public ActionResult customers()
+        {
+            var customer1 = new customer() { name = "naveen" };
+            var customer2 = new customer() { name = "naga" };
+
+
+            return View();
+        }
+    }
+}
---
+++ Vidly1/Models/customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly1.Models
+{
+    public class customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
