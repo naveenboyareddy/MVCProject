6ea03c7706e7fa956365cb1cdac3eff79aaa7cdc
LAPTOP-0H013EO4\Nvn <boyanaveen30@gmail.com>
2017-04-05 16:22:34 -0400

    addingentity

--- Vidly1/Controllers/customerController.cs
+++ Vidly1/Controllers/customerController.cs
@@ -4,34 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly1.Models;
+using System.Data.Entity;
 
 namespace Vidly1.Controllers
 {
     public class customerController : Controller
     {
         // GET: customer
+        private ApplicationDbContext _context;
+        public customerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
-            var customers = getallcustomers();
-            return View(customers);
+            var customers = _context.customers.ToList();
+           return View();
+
         }
 
         public ActionResult details(int id)
         {
-            var customers = getallcustomers().SingleOrDefault(x => x.id == id);
-            if(customers == null)
-            return HttpNotFound();
+            var customers = (_context.customers).SingleOrDefault(x => x.id == id);
+            if (customers == null)
+                return HttpNotFound();
 
             return View(customers);
 
         }
-        private IEnumerable<customer> getallcustomers()
-        {
-             return new List<customer>
-            {
-               new customer { id = 101, name ="Naveen"},
-               new customer { id = 102, name = "Naga"}
-            };
-        }
     }
+       
 }
