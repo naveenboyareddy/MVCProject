bae368c370e50c420c9ecd7f976cc44220598df9
LAPTOP-0H013EO4\Nvn <boyanaveen30@gmail.com>
2017-04-07 17:45:20 -0400

    cc

--- Vidly1/Models/Movie.cs
+++ Vidly1/Models/Movie.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace Vidly1.Models
 {
     public class Movie
     {
+
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        [Required]
+
         public Genre Genre { get; set; }
-        public int GenreId { get; set; }
+
+        [Display(Name = "Genre")]
+        [Required]
+        public byte GenreId { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
-        public byte NumbersInStock { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
+       
 
         //This is data that represents state
     }
