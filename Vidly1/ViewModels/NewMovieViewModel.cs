9591271f9fc81a2d30ca3a8f63a4fb0a7fcb3a47
LAPTOP-0H013EO4\Nvn <boyanaveen30@gmail.com>
2017-04-07 17:46:56 -0400

    cc

---
+++ Vidly1/ViewModels/NewMovieViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly1.Models;
+
+namespace Vidly1.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> genres { set; get; }
+        public Movie movies { get; set; }
+        public string Title
+        {
+            get
+            {
+                     if(movies != null && movies.Id !=0)
+
+                return "Edit Movie";   
+
+                    return "New Movie";
+            }
+        }
+    }
+}
