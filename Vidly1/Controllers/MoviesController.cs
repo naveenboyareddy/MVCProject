faa61104908aef604f3897631c06b0d17c8ac75f
LAPTOP-0H013EO4\Nvn <boyanaveen30@gmail.com>
2017-04-07 17:41:17 -0400

    c

--- Vidly1/Controllers/MoviesController.cs
+++ Vidly1/Controllers/MoviesController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Vidly1.Migrations;
 using Vidly1.Models;
 using Vidly1.ViewModels;
-using System.Data.Entity;
+
 
 namespace Vidly1.Controllers
 {
@@ -21,8 +24,51 @@ namespace Vidly1.Controllers
         {
             _context.Dispose();
         }
+        public ActionResult New()
+        {
+            var genres = _context.genres.ToList();
+            var viewmodel = new MovieFormViewModel
+            {
+                genres = genres
+            };
+            return View("MovieForm", viewmodel);
+        }
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.movies.SingleOrDefault(c => c.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+            var viewmodel = new MovieFormViewModel
+            {
+                movies = movie,
+                genres = _context.genres.ToList()
+            };
+            return View("MovieForm", viewmodel);
+        }
+          [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+              
+                _context.movies.Add(movie);
+            }
+            else
+            {
+                var movieindb = _context.movies.Single(c => c.Id == movie.Id);
 
-
+                movieindb.Name = movie.Name;
+                movieindb.ReleaseDate = movie.ReleaseDate;
+                movieindb.GenreId = movie.GenreId;
+                //movieindb.NumbersInStock = movie.NumbersInStock;
+            }
+           
+                _context.SaveChanges();
+            
+            
+            return RedirectToAction("Index", "Movies");
+        }
         public ViewResult Index()
         {
             var movies = _context.movies.Include(c => c.Genre).ToList();
